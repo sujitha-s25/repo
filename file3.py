4f9541957e3eaf31e42147de708b3575a2235091
Sujitha <sujitha.s@zilogic.com>
2025-07-23 11:28:14 +0530

    Refactor : Added third variable file3

--- file3.py
+++ file3.py
@@ -2,7 +2,7 @@ from file1 import get_input
 from file2 import add
 def main():
     a,b = get_input()
-    result = add(a,b)
+    result = add(a,b,c)
     print("sum is" ,result)
 if __name__ == "__main__":
     main()
